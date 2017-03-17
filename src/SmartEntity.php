bf8f72c5b5e3c6a04451a0a90c329c0e79e61663
Martin Kapal <flamecze@gmail.com>
2017-03-17 14:56:43 +0100

    added SmartEntity using Nette\SmartObject

---
+++ src/SmartEntity.php
@@ -0,0 +1,261 @@
+<?php
+/**
+ * @author Martin Kapal <flamecze@gmail.com>
+ * @author Tomáš Korený <tom@koreny.eu>
+ */
+
+namespace Lempls\SmartEntity;
+
+use Doctrine\ORM\EntityNotFoundException;
+use Doctrine\ORM\Mapping as ORM;
+use Nette\SmartObject;
+
+/**
+ * @ORM\MappedSuperclass()
+ */
+class SmartEntity
+{
+
+    use SmartObject;
+
+    /**
+     * Find all entities
+     *
+     * @return array
+     */
+    public static function all() {
+        return \EntityManager::getRepository(get_called_class())->findAll();
+    }
+
+    /**
+     * Finds one or creates new entity.
+     *
+     * @param array $options
+     * @return object
+     */
+    public static function firstOrNew($options = []) {
+        $entity = self::findOne($options);
+        if (!$entity) {
+            $entity = self::create($options);
+        }
+        return $entity;
+    }
+
+    /**
+     * Finds array of entities matching filter, or id, or array of id's
+     *
+     * @param array $options
+     * @return array|object
+     */
+    public static function find($options = [])
+    {
+        if (gettype($options) === 'integer') {
+            return \EntityManager::find(get_called_class(), $options);
+        } elseif (self::has_string_keys($options) || $options === []) {
+            return \EntityManager::getRepository(get_called_class())->findBy($options);
+        } else {
+            $array = [];
+            foreach ($options as $option) {
+                $array[] = \EntityManager::find(get_called_class(), $option);
+            }
+            return $array;
+        }
+
+    }
+
+    /**
+     * Helper function to determine if array has string keys.
+     *
+     * @param array $array
+     * @return bool
+     */
+    private static function has_string_keys(array $array) {
+        return count(array_filter(array_keys($array), 'is_string')) > 0;
+    }
+
+    /**
+     * Finds exactly one entity matching filter.
+     *
+     * @param array|int $options
+     * @return object
+     */
+    public static function findOne($options = [])
+    {
+        if (gettype($options) === 'integer') {
+            return \EntityManager::find(get_called_class(), $options);
+        }
+        else {
+            return \EntityManager::getRepository(get_called_class())->findOneBy($options);
+        }
+    }
+
+
+    /**
+     * Finds object by filter, if doesn't exist, creates it, and updates it with second parameter.
+     *
+     * @param       $options
+     * @param array $update
+     * @param bool  $allow_empty_strings
+     * @return object
+     */
+    public static function updateOrCreate($options, array $update, bool $allow_empty_strings = true)
+    {
+        $entity = self::firstOrNew($options);
+        foreach ($update as $key => $value) {
+            if (!$allow_empty_strings && $value === "") {
+                continue;
+            }
+            $entity->__set($key, $value);
+        }
+        $entity->save();
+        return $entity;
+    }
+
+    /**
+     * Deletes itself.
+     */
+    public function delete()
+    {
+        \EntityManager::remove($this);
+        \EntityManager::flush($this);
+    }
+
+    /**
+     * Mass delete, same params as find.
+     *
+     * @param array|int $options
+     */
+    public static function destroy($options)
+    {
+        $entities = self::find($options);
+        if (gettype($entities) === 'array') {
+            foreach ($entities as $entity) {
+                $entity->delete();
+            }
+        } else {
+            $entities->delete();
+        }
+    }
+
+    /**
+     * Creates new entity, persists to database.
+     *
+     * @param array $options
+     * @return object
+     */
+    public static function firstOrCreate($options = [])
+    {
+        $entity = self::firstOrNew($options);
+        $entity->save();
+        return $entity;
+    }
+
+
+    /**
+     * Find array of entities or throw error.
+     *
+     * @param array  $options
+     * @param string $error_message
+     * @return array
+     * @throws EntityNotFoundException
+     */
+    public static function findOrFail($options = [], $error_message = 'No entity found matching this filter.')
+    {
+        $return = self::find($options);
+        if ($return === [] || $return == null) throw new EntityNotFoundException($error_message);
+        return $return;
+    }
+
+
+    /**
+     * Find one entity or throw error.
+     *
+     * @param array  $options
+     * @param string $error_message
+     * @return object
+     * @throws EntityNotFoundException
+     */
+    public static function firstOrFail($options = [], $error_message = 'No entity found matching this filter.')
+    {
+        $return = self::findOne($options);
+        if ($return === null) throw new EntityNotFoundException($error_message);
+        return $return;
+    }
+
+    /**
+     * Saves entity.
+     */
+    public function save()
+    {
+        \EntityManager::persist($this);
+        \EntityManager::flush($this);
+    }
+
+
+    /**
+     * Creates new entity base on options.
+     *
+     * @param      $options
+     * @param bool $allow_empty_strings
+     * @return static
+     */
+    public static function create($options, bool $allow_empty_strings = true)
+    {
+        $entity = new static;
+        if (gettype($options) === 'array') {
+            foreach ($options as $key => $value) {
+                if (!$allow_empty_strings && $value === "") {
+                    continue;
+                }
+                $entity->__set($key, $value);
+            }
+        }
+        return $entity;
+    }
+
+    /**
+     * Fetches all records like $key => $value pairs
+     *
+     * @param array|string $criteria parameter can be skipped
+     * @param string $value mandatory
+     * @param array|string $orderBy parameter can be skipped
+     * @param string $key optional
+     * @return array
+     * @throws \Exception
+     */
+    public static function findPairs($criteria, $value = NULL, $orderBy = [], $key = NULL)
+    {
+        if (!is_array($criteria)) {
+            $key = $orderBy;
+            $orderBy = $value;
+            $value = $criteria;
+            $criteria = [];
+        }
+
+        if (!is_array($orderBy)) {
+            $key = $orderBy;
+            $orderBy = [];
+        }
+
+        if (empty($key)) {
+            $key = 'id';
+        }
+
+        $query = \EntityManager::getRepository(get_called_class())->createQueryBuilder('e')
+//            ->whereCriteria($criteria)
+            ->select("e.$value", "e.$key")
+            ->resetDQLPart('from')->from(get_called_class(), 'e', 'e.' . $key)
+//            ->orderBy((array) $orderBy)
+            ->getQuery();
+
+        try {
+            return array_map(function ($row) {
+                return reset($row);
+            }, $query->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY));
+
+        } catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
+
+}
